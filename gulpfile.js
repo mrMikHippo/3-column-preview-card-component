896ea622af967c43dbac622bc45142752cc3b7e6
mrMikOctoM <example@example.com>
2021-04-13 13:08:54 +0300

    Added gulp tasks for css and html [part 1]. And moved src files to src directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,41 @@
-const { series } = require("gulp");
+const { series, src, dest } = require("gulp");
+const postcss = require('gulp-postcss');
+const rename = require('gulp-rename');
 
-function minify(cb) {
-  cb();
+const paths = {
+  src: 'src/',
+  dev: 'dev/',
+  prod: 'prod/'
+};
+
+function css() {
+  return src(paths.src + 'css/styles.css')
+    .pipe(dest(paths.dev + 'css/'));
 }
 
-function transplite(cb) {
-  cb();
+function production_css(cb) {
+  return src(paths.src + 'css/styles.css')
+    .pipe(postcss())
+    .pipe(rename({
+      extname: ".min.css"
+    }))
+    .pipe(dest(paths.prod + 'css/'));
 }
-
-
 
 function clean(cb) {
-
   cb();
 }
 
-function build(cb) {
-  cb();
+function html() {
+  return src(paths.src + '*.html')
+    .pipe(dest(paths.dev));
 }
 
-exports.build = build;
-exports.default = series(clean, build);
+function production_html() {
+  return src(paths.src + '*.html')
+    .pipe(dest(paths.prod));
+}
+
+exports.build = series(production_css, production_html);
+exports.default = series(css, html);
+// exports.default = series(clean, build);
